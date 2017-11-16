1fa1565ef8897efd453a4f511f13ab641c95e8b7
halarotu <hannes.alarotu@gmail.com>
2017-11-16 20:23:24 +0200

    controller added

---
+++ src/main/java/DropboxLoadingApplication.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropboxLoadingApplication {
+
+
+        public static void main(String[] args) throws Exception {
+            SpringApplication.run(DropboxLoadingApplication.class, args);
+        }
+}
+
